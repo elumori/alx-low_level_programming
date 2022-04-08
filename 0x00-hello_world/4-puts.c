7bbe1872f73d0e69f9f5c0f82f0e9f77e6d0fba2
elumori <elumori8257@gmail.com>
2022-04-08 00:54:24 -0700

    4-puts,

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * programme to print muli-lingual puzzle
+ * return 0 after pprinting the function
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle")
+	return (0);
+}
