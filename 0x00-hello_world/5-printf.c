cb5d7d4aad72db6f433cdb69d7729aa93c9204a6
elumori <elumori8257@gmail.com>
2022-04-08 02:26:24 -0700

    5-printf c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *My hello, print task
+ * Return: retun 1 After program execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
