9d352d3c34f2e488d9df7abdd645064d30a4ea7e
elumori <elumori8257@gmail.com>
2022-04-12 07:25:04 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include<main.h>
+#include<stdio.h>
+/**
+ * main - print alphabet x10
+ *
+ * Return: Always 0 on success
+ */
+void print_alphabet_x10(void)
+{
+	int alphabet, count;
+
+	count = 0;
+	while (count < 10)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+
+		count++;
+		_putchar('\n');
+	}
+}
