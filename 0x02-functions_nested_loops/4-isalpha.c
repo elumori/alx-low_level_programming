7a0f0da03a7ed38bf96bdbd970819860058a7e33
elumori <elumori8257@gmail.com>
2022-04-12 07:41:31 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include<main.h>
+#include<stdio.h>
+/**
+ * main - checks for alpha char
+ *
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+
+	}
+
+}
