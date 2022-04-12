0b70ae411323702166646f73e22fbe28bbf4e031
elumori <elumori8257@gmail.com>
2022-04-12 07:13:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include<main.h>
+#include<stdio.h>
+/**
+ * main - print alphabets a-z
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar('alphabet');
+	}
+
+	_putchar('\n');
+
+}
