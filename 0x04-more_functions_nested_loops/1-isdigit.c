e0db1ed529396c3627ec6ffc1b3c9bc9d9cdd3d5
elumori <elumori8257@gmail.com>
2022-07-07 15:22:05 +0200

    commiting _isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_isdigit - checks is character id a digit.
+ *
+ * Return: 1 for digit 0 else
+ */
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
+
