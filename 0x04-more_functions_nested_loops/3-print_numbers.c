7140c36e105472e5e2139149b67d5fc46b374821
elumori <elumori8257@gmail.com>
2022-07-07 15:55:40 +0200

    print_numbers commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - prints number 0-9.
+ *@i: print int
+ *Return: Always 0
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+
+}
