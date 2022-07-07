ddd7e971072e80b1f2b5285b2f51fdfd10e2b3b5
elumori <elumori8257@gmail.com>
2022-07-07 14:46:13 +0200

    comminting more functions and loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,20 @@
 #include "main.h>
 /**
  * _isupper - checks whether a character is upper
- *
+ *@c: print int
  * Return: 1 on success 
  */
-int _isupper(int c){
-	if(x >= 65 && x <= 90){
-		return (1);
-	}
-		return (0);
+
+int _isupper(int c)
+{
+if(c >= 'A' && x <= 'z')
+{
+	return (1);
+}
+
+else
+{
+	return (0);
+}
+
 }
