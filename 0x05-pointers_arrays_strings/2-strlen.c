2dbf6aa43aeaeac3f1b86e0ca9da320fb445fb0d
elumori <elumori8257@gmail.com>
2022-07-12 15:16:25 +0200

    string length commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ * @len: stores variable for the length.
+ *
+ * Return: Always 0 .
+ */
+
+int _strlen(char *s)
+{
+int len;
+
+len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+return (len);
+}
