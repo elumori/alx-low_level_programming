6a5e260453411d31a511b8c89a87f33e613e6798
elumori <elumori8257@gmail.com>
2022-07-13 16:16:09 +0200

    6-puts2 commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - pints every character of a string
+ * @str: A pointer to the int string
+ *
+ *Return: Nothing
+ */
+
+void puts2(char *str)
+{
+int a;
+
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a %2 == 0)
+_putchar (str[a]);
+}
+
+_putchar ('\n');
+}
