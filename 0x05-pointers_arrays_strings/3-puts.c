a8124188da7bf797ea25608bd8f80f0499574578
elumori <elumori8257@gmail.com>
2022-07-13 08:55:56 +0200

    3-puts commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _puts - prints a string of chars.
- * @str: a pointer to an int that will change to string
- *
- * Return: Nothing.
- */
+*_puts - prints a string
+*@str: A pointer to an int that will be changed
+*
+*Return: void which means our answer is correct
+*/
 
 void _puts(char *str)
 {
