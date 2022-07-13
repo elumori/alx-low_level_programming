df4b204bcc912483579821320ce30841c6cf0858
elumori <elumori8257@gmail.com>
2022-07-13 09:55:08 +0200

    3-puts commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,17 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
-*_puts - prints a string
-*@str: A pointer to an int that will be changed
-*
-*Return: void which means our answer is correct
-*/
+ *_puts - shows the pointer of the var
+ *@str: string pointer
+ *
+ */
 
 void _puts(char *str)
 {
-char *c;
-int m;
-
-c = str;
-
-for (m = 0; c[m]; m++)
-{
-_putchar (c[m]);
-}
-_putchar('\n');
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
