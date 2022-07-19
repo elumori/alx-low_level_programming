6c9b775236ca519976017ad63c0733e4c2083652
elumori <elumori8257@gmail.com>
2022-07-19 09:38:23 +0200

    0x07 more commits

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)  /*Delacring WHILE*/
+	{
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
+		i++;
+
+	} /*END WHILE*/
+
+
+	return (dest);
+}
