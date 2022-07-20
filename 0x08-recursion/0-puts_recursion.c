edddb5046ee4565f01946267fec4e471dff97093
elumori <elumori8257@gmail.com>
2022-07-20 13:58:15 +0200

    task 0x08 recursion commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints string
+ *
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	char *s;
+	s = "puts with recursion";
+	_puts_recursion(s);
+}
