f49127ef12073dad9d82d12abdba9567b3192be2
elumori <elumori8257@gmail.com>
2022-08-02 15:23:34 +0200

    grid.c commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,43 +1,20 @@
 #include "main.h"
 /**
- * alloc_grid - allocates a grid, make space and free space
- * @width: takes in width of grid
+ * free_grid - allocates a grid, make space and free space
+ * @grid: takes in width of grid
  * @height: height of grid
- * Return: grid with freed spaces
+ * Return: free grid
  */
 
-int **alloc_grid(int width, int height)
+void free_grid(int **grid, int height)
 {
-/*Declaring variables*/
-int **grid;
-int i, j;
+	int i;
 
-if (width <= 0 || height <= 0)
-{
-return (NULL);
-}
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
 
-grid = malloc(sizeof(int *) * height); /*malloc*/
-
-if (grid == NULL)
-{
-return (NULL);
+	free(grid);
 }
 
-for (i = 0; i < height; i++)
-{
-grid[i] = malloc(sizeof(int) * width);
-if (grid[i] == NULL)
-{
-for (i = i - 1; i >= 0; i--)
-{
-free(grid[i]);
-}
-free(grid);
-return (NULL);
-}
-}
-for (i = 0; j < width; j++)
-grid[i][j] = 0;
-return (grid);
-}
