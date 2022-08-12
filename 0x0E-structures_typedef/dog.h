65a011cd937c8d0355faa5691593e42610d57101
elumori <elumori8257@gmail.com>
2022-08-12 13:48:09 +0200

    dog struct commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * struct dog - initialises structure for type dog
+ * @name: name of the dog
+ * @age: variable age for dog
+ * @owner: variable owner of the dog
+ * Description: this struct is for dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
